7b880111745f26235a22031a997563804677ac36
Francisco Fuentes <fran_fuentes20@hotmail.com>
2015-12-21 01:26:16 +0100

    Añadida capacidad del organizador de comprobar indices​

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -10,7 +10,7 @@ public class MusicOrganizer
 {
     // An ArrayList for storing the file names of music files.
     private ArrayList<String> files;
-        
+
     /**
      * Create a MusicOrganizer
      */
@@ -18,7 +18,7 @@ public class MusicOrganizer
     {
         files = new ArrayList<String>();
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -27,7 +27,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -36,7 +36,7 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
@@ -48,7 +48,7 @@ public class MusicOrganizer
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
@@ -57,6 +57,16 @@ public class MusicOrganizer
     {
         if(index >= 0 && index < files.size()) {
             files.remove(index);
+        }
+    }
+
+    /**
+     * Método que permite comprobar si el índice introducido esta dentro de un rango válido
+     */
+    public void checkIndex(int index){
+        if (index >= 0 && index < files.size()){
+            System.out.println("El índice introducido no es correcto");
+            System.out.println("Se debe introducir un numero desde 0 a " + (files.size() - 1));
         }
     }
 }
